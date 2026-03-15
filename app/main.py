4c62a5ddd09c95c33170a60f0e155e29db979d79
Abhijeet Raj <57141191+abhijeetraj22@users.noreply.github.com>
2026-03-15 18:42:50 +0530

    Add files via upload

---
+++ app/main.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import requests
+import os
+from fastapi.middleware.cors import CORSMiddleware
+app = FastAPI()
+
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+REPO_OWNER = "abhijeetraj22"
+REPO_NAME = "daily_report"
+
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class Report(BaseModel):
+    date: str
+    image: str
+
+@app.post("/save-report")
+def save_report(data: Report):
+
+    file_path = f"reports/{data.date}.png"
+
+    url = f"https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/contents/{file_path}"
+
+    headers = {
+        "Authorization": f"token {GITHUB_TOKEN}",
+        "Accept": "application/vnd.github+json"
+    }
+
+    sha = None
+
+    r = requests.get(url, headers=headers)
+
+    if r.status_code == 200:
+        sha = r.json()["sha"]
+
+    payload = {
+        "message": f"Save report {data.date}",
+        "content": data.image,
+    }
+
+    if sha:
+        payload["sha"] = sha
+
+    response = requests.put(url, headers=headers, json=payload)
+
+    return {"status": "saved"}
